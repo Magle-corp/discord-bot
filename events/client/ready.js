1aea92870c1d883a90f197a5b45a3085527fe3c2
Magle-corp <wolff.kevin@protonmail.com>
2023-02-12 14:40:06 +0100

    Github workflow deploy production

--- events/client/ready.js
+++ events/client/ready.js
@@ -39,7 +39,7 @@ module.exports = {
           const data = await rest.put(
             Routes.applicationGuildCommands(
               process.env.CLIENT_ID,
-              process.env.DEV_GUILD_ID
+              process.env.GUILD_ID
             ),
             { body: commands }
           );
